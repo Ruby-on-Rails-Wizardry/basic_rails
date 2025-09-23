0661834526da2309f686db05ba2d0b9b7229c169
Robert Ferney <rob@ferney.org>
2025-09-22 19:39:19 -0600

    new user registration, using slice.permit

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,4 @@
-class RegistrationsController
+class RegistrationsController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
@@ -6,5 +6,10 @@ class RegistrationsController
   end
 
   def create
+    if User.create(params.slice(:email_address, :password, :password_confirmation).permit!)
+      redirect_to new_session_path
+    else
+      redirect_to new_registration_path, alert: "Try another email address or password."
+    end
   end
 end
